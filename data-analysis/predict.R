fed6bd0e438558997766cb92865ae5fd42cf9c74
Ahmad Rajeh <ahmadrjeh@gmail.com>
2016-10-23 00:58:59 -0500

    Update predict.R

--- data-analysis/predict.R
+++ data-analysis/predict.R
@@ -1,5 +1,7 @@
 # usage:
 # Rscript predict.R bal household education state family/friends credit loans layoff savings
+load('model.Rda')
+
 args <- commandArgs(TRUE)
 a <- as.double(args[1])
 b <- as.double(args[2])
@@ -14,8 +16,6 @@ i <- as.double(args[9])
 
 states.ranks <- cbind(read.table('states.txt'), 1:54)
 d <- as.vector(unlist(states.ranks[states.ranks$V1==d.tmp,][2]))
-
-load('model.Rda')
 
 test <- rbind(data.frame(), c(a,b,c,d,e,f,g,h,i))
 colnames(test) <- names(model$coefficients)[-1]
